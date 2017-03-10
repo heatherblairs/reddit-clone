db58e7384fcc6deda9346b2675230ca032b27117
Heather Stone <heathers@US005-LT-0015.local>
2017-03-10 13:32:15 -0700

    factory working

--- components/main.ctrl.js
+++ components/main.ctrl.js
@@ -1,36 +1,31 @@
-// better
-  (function(){
-    "use strict";
-    angular
-       .module("campSpots")
-          .controller("mainCtrl", mainCtrl);
+(function(){
+  "use strict";
+  angular
+    .module("campSpots")
+    .controller("mainCtrl", function($scope, $http, postFactory){
+      postFactory.getPosts().then(function(posts){
+        $scope.posts = posts.data;
+      })
+    })
+})();
 
-          mainCtrl.$inject = ['$scope', '$http', 'postFactory'];
-
-          function mainCtrl($scope, $http, postFactory){
-        postFactory.getPosts().then(function(posts){
-          $scope.posts = posts.data;
-        })
-      }
-  })();
-
-// best
-  // (function(){
-  //   "use strict";
-  //   angular
-  //     .module("campSpots")
-  //     .controller("mainCtrl", mainCtrl);
-  //
-  //     mainCtrl.$inject = ['$scope', '$http', 'postFactory'];
-  //
-  //     function mainCtrl($scope, $http, postFactory){
-  //       var vm = this;
-  //
-  //       vm.post;
-  //       vm.posts;
-  //
-  //       postFactory.getPosts().then(function(posts){
-  //         vm.posts = posts.data;
-  //       })
-  //     }
-  // })();
+// (function(){
+//   "use strict";
+//   angular
+//     .module("campSpots")
+//     .controller("mainCtrl", mainCtrl);
+//
+//     mainCtrl.$inject = ['$scope', '$http', 'postFactory'];
+//
+//     function mainCtrl($scope, $http, postFactory){
+//       var vm = this;
+//
+//       vm.post;
+//       vm.posts;
+//
+//       postFactory.getPosts().then(function(posts){
+//         vm.posts = posts.data;
+//         console.log(vm.posts);
+//       })
+//     }
+// })();
